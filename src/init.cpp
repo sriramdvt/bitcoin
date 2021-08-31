35830eba2e48720f657a802c27ef4d1216cee7b0
Sriram <sriramdvt@gmail.com>
2021-09-01 00:48:50 +0530

    Make mempool a nullptr to check PeerManager changes

--- src/init.cpp
+++ src/init.cpp
@@ -1200,7 +1200,8 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
 
     assert(!node.mempool);
     int check_ratio = std::min<int>(std::max<int>(args.GetArg("-checkmempool", chainparams.DefaultConsistencyChecks() ? 1 : 0), 0), 1000000);
-    node.mempool = std::make_unique<CTxMemPool>(node.fee_estimator.get(), check_ratio);
+    // node.mempool is already nullptr, but declaring it explicitly here to make it clear while testing
+    node.mempool = nullptr;
 
     assert(!node.chainman);
     node.chainman = std::make_unique<ChainstateManager>();
@@ -1376,7 +1377,7 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
             const int64_t load_block_index_start_time = GetTimeMillis();
             try {
                 LOCK(cs_main);
-                chainman.InitializeChainstate(Assert(node.mempool.get()));
+                chainman.InitializeChainstate(node.mempool.get());
                 chainman.m_total_coinstip_cache = nCoinCacheUsage;
                 chainman.m_total_coinsdb_cache = nCoinDBCache;
 
